f741a144dc6f16364a1bc4faafca185c439b5fe9
FLAGLEE <1137665564@qq.com>
2020-07-23 16:10:52 +0800

    # 输入某年某月，判断这一天是这一年的第几天

--- doy.py
+++ doy.py
@@ -2,7 +2,7 @@
 import datetime
 
 dtstr = input('Enter the datetime:(20200723):')
-dt = datetime.datetime.strptime(dtstr,"%Y%m%d")
-another_dtstr = dtstr=dtstr[:4]+'0101'
-another_dt = datetime.datetime.strptime(another_dtstr,"%Y%m%d")
-print(int((dt-another_dt).days)+1)
+dt = datetime.datetime.strptime(dtstr, "%Y%m%d")
+another_dtstr = dtstr = dtstr[:4] + '0101'
+another_dt = datetime.datetime.strptime(another_dtstr, "%Y%m%d")
+print(int((dt - another_dt).days) + 1)
