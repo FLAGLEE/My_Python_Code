0d4c5313c408713991b133cd72b69c9c338b2f74
FLAGLEE <1137665564@qq.com>
2020-07-29 16:00:55 +0800

    # doy<=>date GUI程序

--- doy.py
+++ doy.py
@@ -1,5 +1,7 @@
 # doy 与 date 相互转换
 import datetime
+import easygui as g
+import sys
 
 
 def doy2data(year, doy):
@@ -36,45 +38,73 @@ def doy2data(year, doy):
     return date
 
 
-flag = 1
-while flag == 1:
-    n = int(input('what do you want to do?\n'
-                  '1 date2doy\n'
-                  '2 doy2date\n'
-                  'input:'))
-    if n == 1:
-        dtstr = input('Enter the datetime(yyyymmdd:20200723):')
+# GUI
+while 1:
+    msg = "what do you want to do?"
+    title = "doy<=>date"
+    choices = ["date2doy", "doy2date"]
+    choice = g.choicebox(msg, title, choices)
+
+    if choice == "date2doy":
+        outdtstr = dtstr = g.enterbox('Enter the datetime(yyyymmdd:20200723):')
+        print(dtstr)
         dt = datetime.datetime.strptime(dtstr, "%Y%m%d")
         another_dtstr = dtstr = dtstr[:4] + '0101'
         another_dt = datetime.datetime.strptime(another_dtstr, "%Y%m%d")
-        print('The date to doy is', int((dt - another_dt).days) + 1, '\n')
-        flag = int(input('Do you still want to calculate?\n'
-                         '1 yes\n'
-                         '0 no\n'
-                         'input:'))
-        while flag != 1 and flag != 0:
-            print('Input error, please retype')
-            flag = int(input('Do you still want to calculate?\n'
-                             '1 yes\n'
-                             '0 no\n'
-                             'input:'))
-        continue
-    elif n == 2:
-        year = int(input('Enter the year(yyyy:2020):'))
-        doy = int(input(('Enter the doy(ddd:192):')))
-        print('The', doy, 'to date is :', doy2data(year, doy), '\n')
-        flag = int(input('Do you still want to calculate?\n'
-                         '1 yes\n'
-                         '0 no\n'
-                         'input:'))
-        while flag != 1 and flag != 0:
-            print('Input error, please retype')
-            flag = int(input('Do you still want to calculate?\n'
-                             '1 yes\n'
-                             '0 no\n'
-                             'input:'))
-        continue
+        g.msgbox('The ' + outdtstr + ' to doy is ' + str(int((dt - another_dt).days) + 1), "结果", ok_button="确定")
+    elif choice == "doy2date":
+        year = int(g.enterbox('Enter the year(yyyy:2020):'))
+        doy = int(g.enterbox('Enter the doy(ddd:192):'))
+        date = doy2data(year, doy)
+        g.msgbox('The ' + str(doy) + ' to date is: ' + str(date), "结果", ok_button="确定")
+
+    msg = "你希望继续计算吗？"
+    title = "请选择..."
+    if g.ccbox(msg, title):
+        pass
     else:
-        print('Input error, please retype')
-        continue
-print('see you~')
+        sys.exit(0)
+
+# # 源码
+# flag = 1
+# while flag == 1:
+#     n = int(input('what do you want to do?\n'
+#                   '1 date2doy\n'
+#                   '2 doy2date\n'
+#                   'input:'))
+#     if n == 1:
+#         dtstr = input('Enter the datetime(yyyymmdd:20200723):')
+#         dt = datetime.datetime.strptime(dtstr, "%Y%m%d")
+#         another_dtstr = dtstr = dtstr[:4] + '0101'
+#         another_dt = datetime.datetime.strptime(another_dtstr, "%Y%m%d")
+#         print('The date to doy is', int((dt - another_dt).days) + 1, '\n')
+#         flag = int(input('Do you still want to calculate?\n'
+#                          '1 yes\n'
+#                          '0 no\n'
+#                          'input:'))
+#         while flag != 1 and flag != 0:
+#             print('Input error, please retype')
+#             flag = int(input('Do you still want to calculate?\n'
+#                              '1 yes\n'
+#                              '0 no\n'
+#                              'input:'))
+#         continue
+#     elif n == 2:
+#         year = int(input('Enter the year(yyyy:2020):'))
+#         doy = int(input(('Enter the doy(ddd:192):')))
+#         print('The', doy, 'to date is :', doy2data(year, doy), '\n')
+#         flag = int(input('Do you still want to calculate?\n'
+#                          '1 yes\n'
+#                          '0 no\n'
+#                          'input:'))
+#         while flag != 1 and flag != 0:
+#             print('Input error, please retype')
+#             flag = int(input('Do you still want to calculate?\n'
+#                              '1 yes\n'
+#                              '0 no\n'
+#                              'input:'))
+#         continue
+#     else:
+#         print('Input error, please retype')
+#         continue
+# print('see you~')
