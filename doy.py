394ae37f6c849cd86319ad051b692a605cb7a9fd
FLAGLEE <1137665564@qq.com>
2020-07-29 21:50:18 +0800

    # doy 与 date 相互转换

--- doy.py
+++ doy.py
@@ -1,8 +1,11 @@
 # doy 与 date 相互转换
-import datetime
-import easygui as g
-import sys
+from datetime import datetime
+from easygui import msgbox, enterbox, ccbox, choicebox
+from sys import exit
 
+
+# import easygui as g
+# import datetime
 
 def doy2data(year, doy):
     month_leapyear = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
@@ -43,27 +46,27 @@ while 1:
     msg = "what do you want to do?"
     title = "doy<=>date"
     choices = ["date2doy", "doy2date"]
-    choice = g.choicebox(msg, title, choices)
+    choice = choicebox(msg, title, choices)
 
     if choice == "date2doy":
-        outdtstr = dtstr = g.enterbox('Enter the datetime(yyyymmdd:20200723):')
+        outdtstr = dtstr = enterbox('Enter the datetime(yyyymmdd:20200723):')
         print(dtstr)
-        dt = datetime.datetime.strptime(dtstr, "%Y%m%d")
+        dt = datetime.strptime(dtstr, "%Y%m%d")
         another_dtstr = dtstr = dtstr[:4] + '0101'
-        another_dt = datetime.datetime.strptime(another_dtstr, "%Y%m%d")
-        g.msgbox('The ' + outdtstr + ' to doy is ' + str(int((dt - another_dt).days) + 1), "结果", ok_button="确定")
+        another_dt = datetime.strptime(another_dtstr, "%Y%m%d")
+        msgbox('The ' + outdtstr + ' to doy is ' + str(int((dt - another_dt).days) + 1), "结果", ok_button="确定")
     elif choice == "doy2date":
-        year = int(g.enterbox('Enter the year(yyyy:2020):'))
-        doy = int(g.enterbox('Enter the doy(ddd:192):'))
+        year = int(enterbox('Enter the year(yyyy:2020):'))
+        doy = int(enterbox('Enter the doy(ddd:192):'))
         date = doy2data(year, doy)
-        g.msgbox('The ' + str(doy) + ' to date is: ' + str(date), "结果", ok_button="确定")
+        msgbox('The ' + str(doy) + ' to date is: ' + str(date), "结果", ok_button="确定")
 
     msg = "你希望继续计算吗？"
     title = "请选择..."
-    if g.ccbox(msg, title):
+    if ccbox(msg, title):
         pass
     else:
-        sys.exit(0)
+        exit(0)
 
 # # 源码
 # flag = 1
