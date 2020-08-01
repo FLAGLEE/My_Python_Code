d2306c91f740c8e582a181be69362102a84d1aa4
FLAGLEE <1137665564@qq.com>
2020-08-01 22:10:59 +0800

    # 定义类 @property setter

--- learn_Class.py
+++ learn_Class.py
@@ -148,3 +148,41 @@ if s.resolution == 786432:
     print('测试通过!')
 else:
     print('测试失败!')
+
+
+class Person(object):
+    # 限定Person对象只能绑定_name, _age和_gender属性
+    __slots__ = ('_name', '_age', '_gender')
+
+    def __init__(self, name, age):
+        self._name = name
+        self._age = age
+
+    # 访问器 - getter方法
+    @property
+    def name(self):
+        return self._name
+
+    # 访问器 - getter方法
+    @property
+    def age(self):
+        return self._age
+
+    # 修改器 - setter方法
+    @age.setter
+    def age(self, age):
+        self._age = age
+
+    def play(self):
+        if self._age <= 16:
+            print('%s正在玩飞行棋.' % self._name)
+        else:
+            print('%s正在玩斗地主.' % self._name)
+
+
+person = Person('王大锤', 12)
+person.play()
+person.age = 22
+person.play()
+# person.name = '白元芳'  # AttributeError: can't set attribute
+
