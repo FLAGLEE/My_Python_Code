78092c88fef1f3af001800e8c3752032a05bc6b6
FLAGLEE <1137665564@qq.com>
2020-08-05 15:07:07 +0800

    # try

--- test_try.py
+++ test_try.py
@@ -1,14 +1,25 @@
-# test try
-try:
-    print('try...')
-    r = 10 / 10
-    print('result:', r)
-except TypeError as e:
-    print('TypeError:', e)
-except ZeroDivisionError as e:
-    print('ZeroDivisionError:', e)
-else:
-    print('No Error!')
-finally:
-    print('finally...')
-print('end try')
+# try
+from functools import reduce
+
+
+def str2num(s):
+    try:
+        return int(s)
+    except:
+        return float(s)
+
+
+def calc(exp):
+    ss = exp.split('+')
+    ns = map(str2num, ss)
+    return reduce(lambda acc, x: acc + x, ns)
+
+
+def main2():
+    r = calc('100 + 200 + 345')
+    print('100 + 200 + 345 =', r)
+    r = calc('99 + 88 + 7.6')
+    print('99 + 88 + 7.6 =', r)
+
+
+main2()
