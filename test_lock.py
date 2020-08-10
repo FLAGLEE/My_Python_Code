b2f68642f55b41a667417e07f4b1f90551e22a39
FLAGLEE <1137665564@qq.com>
2020-08-10 23:37:41 +0800

    # 线程锁

---
+++ test_lock.py
@@ -0,0 +1,38 @@
+# 线程锁
+import time, threading
+
+# 要确保balance计算正确，就要给change_it()上一把锁，当某个线程开始执行change_it()时，该线程因为获得了锁，
+# 其他线程不能同时执行change_it()，只能等待，直到锁被释放后，获得该锁以后才能改。
+# 创建一个锁就是通过threading.Lock()来实现：
+balance = 0
+lock = threading.Lock()
+
+
+def change_it(n):
+    # 先存后取，结果应该为0:
+    global balance
+    balance = balance + n
+    balance = balance - n
+
+
+# 当多个线程同时执行lock.acquire()时，只有一个线程能成功地获取锁，然后继续执行代码，其他线程就继续等待直到获得锁为止。
+# 获得锁的线程用完后一定要释放锁，否则那些苦苦等待锁的线程将永远等待下去，成为死线程。所以我们用try...finally来确保锁一定会被释放。
+def run_thread(n):
+    for i in range(100000):
+        # 先要获取锁:
+        lock.acquire()
+        try:
+            # 放心地改吧:
+            change_it(n)
+        finally:
+            # 改完了一定要释放锁:
+            lock.release()
+
+
+t1 = threading.Thread(target=run_thread, args=(5,))
+t2 = threading.Thread(target=run_thread, args=(8,))
+t1.start()
+t2.start()
+t1.join()
+t2.join()
+print(balance)
