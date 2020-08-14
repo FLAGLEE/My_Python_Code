daedb41cabcb2ad029301868c6a5b74117322e30
FLAGLEE <1137665564@qq.com>
2020-08-14 13:58:38 +0800

    # test pillow

---
+++ test_pillow.py
@@ -0,0 +1,15 @@
+from PIL import Image, ImageFilter
+
+# 打开一个jpg图像文件，注意是当前路径:
+im = Image.open('./src/ball.png')
+# 获得图像尺寸:
+w, h = im.size
+print('Original image size: %sx%s' % (w, h))
+# 缩放到50%:
+im.thumbnail((w // 9, h // 9))
+print('Resize image to: %sx%s' % (w // 9, h // 9))
+# 把缩放后的图像用jpeg格式保存:
+im.save('thumbnail.png', 'png')
+# 应用模糊滤镜:
+im2 = im.filter(ImageFilter.BLUR)
+im2.save('blur.png', 'png')
