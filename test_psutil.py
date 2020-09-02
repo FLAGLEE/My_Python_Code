c02d52ff5deff64db7533a29b1c36de06de4f7c8
FLAGLEE <1137665564@qq.com>
2020-09-02 09:26:35 +0800

    # test psutil

--- test_psutil.py
+++ test_psutil.py
@@ -29,13 +29,27 @@ print(psutil.net_connections())  # 要获取当前网络连接信息
 # 而获取网络连接信息需要root权限，这种情况下，可以退出Python交互环境，用sudo重新启动：
 
 # 通过psutil可以获取到所有进程的详细信息：
-print(psutil.pids()) # 所有进程ID
-print()
-print()
-print()
-print()
-print()
-print()
+print(psutil.pids())  # 所有进程ID
+p = psutil.Process(12944)  # 获取指定进程ID=12944，其实就是当前Python交互环境
+print(p.name())  # 进程名称
+print(p.exe())  # 进程exe路径
+print(p.cwd())  # 进程工作目录
+print(p.cmdline())  # 进程启动的命令行
+print(p.ppid())  # 父进程ID
+print(p.parent())  # 父进程
+print(p.children())  # 子进程列表
+print(p.status())  # 进程状态
+print(p.username())  # 进程用户名
+print(p.create_time())  # 进程创建时间
+# print(p.terminal())  # 进程终端
+print(p.cpu_times())  # 进程使用的CPU时间
+print(p.memory_info())  # 进程使用的内存
+print(p.open_files())  # 进程打开的文件
+print(p.connections())  # 进程相关网络连接
+print(p.num_threads())  # 进程的线程数量
+print(p.threads())  # 所有线程信息
+print(p.environ())  # 进程环境变量
+print(p.terminate())  # 结束进程
 
 # psutil还提供了一个test()函数，可以模拟出ps命令的效果：
 print(psutil.test())
