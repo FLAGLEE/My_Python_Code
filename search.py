e01270670019340d9ec00569162dd2d8a9ab5147
FLAGLEE <1137665564@qq.com>
2020-09-28 10:41:05 +0800

    # 二分查找

---
+++ search.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# 二分查找
+def search(sequence, number, lower=0, upper=None):
+    if upper is None:
+        upper = len(sequence) - 1
+    if lower == upper:
+        assert number == sequence[upper]
+        return upper
+    else:
+        middle = (lower + upper) // 2
+        if number > sequence[middle]:
+            return search(sequence, number, middle + 1, upper)
+        else:
+            return search(sequence, number, lower, middle)
+
+
+seq = [34, 67, 8, 123, 4, 100, 95]
+seq.sort()
+
+print(seq)
+print(search(seq, 34))
+print(search(seq, 100))
